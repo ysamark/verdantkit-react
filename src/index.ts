0aefa85f39c2414180ef3601185cc31bc9c25e6f
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-10-17 14:14:22 +0100

    feat: update tailwind config

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export { Toaster, toast } from "sonner";
 
 export * as Accordion from "@radix-ui/react-accordion";
 export * from "./components";
+export * from "./config/tailwind-plugin";
 export * from "./utils";

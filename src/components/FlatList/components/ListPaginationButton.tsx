e3251450b9b51977fc45891bbdd18ee23570d4ad
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-10-20 08:50:36 +0100

    fix(@components/FlatList): apply theme primary color in hightlighted pagination buttons

--- src/components/FlatList/components/ListPaginationButton.tsx
+++ src/components/FlatList/components/ListPaginationButton.tsx
@@ -21,7 +21,7 @@ export const ListPaginationButton = forwardRef<
       className={cn(
         "bg-transparent w-max border-0 outline-0 py-2 px-3 rounded-full text-zinc-800 bg-zinc-200 dark:bg-zinc-800 dark:text-zinc-100 enabled:hover:bg-zinc-300 enabled:active:bg-zinc-400 dark:enabled:hover:bg-zinc-700 dark:enabled:active:bg-zinc-600 [&_span]:flex",
         props.selected
-          ? "bg-blue-500 enabled:hover:bg-blue-600 enabled:active:bg-blue-700 dark:bg-blue-500 dark:enabled:hover:bg-blue-600 dark:enabled:active:bg-blue-700 text-zinc-50 [&_span]:px-3"
+          ? "bg-primary-200 enabled:hover:bg-primary-300 enabled:active:bg-primary-400 dark:bg-primary-500 dark:enabled:hover:bg-primary-300 dark:enabled:active:bg-primary-400 text-zinc-50 [&_span]:px-3"
           : "[&_span]:px-1",
         props.className
       )}
